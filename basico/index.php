41aeb56a80ed493cd0f7ab9c765b1e466ae8ed55
Kauan Pereira Gonçalves Dos Santos <kauanluceliagabi@gmail.com>
2025-09-02 14:39:01 -0300

    Primeiros códigos PHP

---
+++ basico/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Básico lingragem PHP</title>
+</head>
+<body>
+    <h1>Conceitos Básicos PHP</h1>
+    <ul>
+        <li>
+            <a href="primeiro.php" target="_blank">Primeiro Script PHP</a>
+        </li>
+        <li>
+            <a href="variaveis.php" target="_blank">Variáveis em PHP</a>
+        </li>
+        <li>
+            <a href="operacoes.php" target="_blank">Operações em PHP</a>
+        </li>
+        <li>
+            <a href="get.php" target="_blank">Recebendo Valores por GET</a>
+        </li>
+        <li>
+            <a href="parimpar.php" target="_blank">Verificar Par ou Ímpar</a>
+        </li>
+        <li>
+            <a href="tabuada.php" target="_blank">Tabuada de um Número</a>
+        </li>
+    </ul>
+    <br>
+    <?php 
+       echo "<h2>Informações do Servidor</h2>";
+       echo $_SERVER['SCRIPT_NAME'];
+       echo "<br>";
+       echo $_SERVER['SERVER_NAME'];
+       echo "<br>";
+    ?>
+</body>
+</html>
---
+++ java/aula.js
@@ -0,0 +1,8 @@
+let contador = 0
+while (contador < 5){
+    contador = contador + 1;
+    console.log("contagem: " + contador);
+}
+for (let count = 0; count < 5; count ++ ){
+    console.log("Outro count: " + count)
+}
